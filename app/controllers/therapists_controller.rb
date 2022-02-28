62ee65f23baf31c185362af82a9b5d5bc6d34ebb
Laura Sucher <l.sucher@gmx.de>
2022-02-28 15:46:40 +0200

    Did the index for therapist

--- app/controllers/therapists_controller.rb
+++ app/controllers/therapists_controller.rb
@@ -1,2 +1,5 @@
 class TherapistsController < ApplicationController
+  def index
+    @therapists = User.where(is_therapist: true)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-  belongs_to :therapist, class_name: 'User', foreign_key: 'user_id', optional: true
-  belongs_to :patient, class_name: 'User', foreign_key: 'user_id', optional: true
+  # belongs_to :therapist, class_name: 'User', foreign_key: 'user_id', optional: true
+  # belongs_to :patient, class_name: 'User', foreign_key: 'user_id', optional: true
+
+  has_many :therapist_sessions, class_name: 'Session', foreign_key: 'therapist_id'
+  has_many :patient_sessions, class_name: 'Session', foreign_key: 'patient_id'
 end
