a2a08a9ab88cb95c21a3e91a16954de5c530f160
Ran Milshtein <ranmilshtein@gmail.com>
2022-02-28 12:44:50 +0200

    finished models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-
+  belongs_to :therapist, class_name: 'User', foreign_key: 'user_id', optional: true
+  belongs_to :patient, class_name: 'User', foreign_key: 'user_id', optional: true
 end
