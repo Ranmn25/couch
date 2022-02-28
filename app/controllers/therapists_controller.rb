8a92c4b0f170aaf1cdecdeed526159702331c210
Ran Milshtein <ranmilshtein@gmail.com>
2022-02-28 15:42:22 +0200

    added seed, show for therapist, view for show

--- app/controllers/therapists_controller.rb
+++ app/controllers/therapists_controller.rb
@@ -1,2 +1,11 @@
 class TherapistsController < ApplicationController
+  def show
+    @therapist = User.where(is_therapist: true)
+  end
+
+  private
+
+  def therapist_paramas
+    params.require(:therapist).permit()
+  end
 end
